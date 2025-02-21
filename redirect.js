eb88672530b68be6030f66b33134bd2f2d5bf1c2
kyawhtet-github <152606140+kyawhtet-github@users.noreply.github.com>
2025-02-21 11:21:14 +0000

    test for redirect

---
+++ redirect.js
@@ -0,0 +1,38 @@
+window.onload = function () {
+    var userAgent = navigator.userAgent || navigator.vendor || window.opera;
+
+    var appStoreUrl = "https://apps.apple.com/us/app/supermyan/id6523422437";
+    var playStoreUrl = "https://play.google.com/store/apps/details?id=com.uabfintech.supermyan&hl=en";
+    var fallbackUrl = "https://uatmoe.uabpay.com.mm/home";
+
+    var buttonsDiv = document.getElementById("buttons");
+
+    function createButton(text, url, bgColor, openInNewTab = false) {
+        var button = document.createElement("a");
+        button.href = url;
+        button.className = `block w-64 px-6 py-3 text-white text-lg font-semibold rounded-lg shadow-md ${bgColor} hover:opacity-80 transition`;
+        button.innerText = text;
+        if (openInNewTab) {
+            button.target = "_blank";
+        }
+        buttonsDiv.appendChild(button);
+    }
+
+    if (/iPad|iPhone|iPod/.test(userAgent) && !window.MSStream) {
+        createButton("Download on the App Store", appStoreUrl, "bg-blue-600");
+    } else if (/android/i.test(userAgent)) {
+        // Try opening Play Store
+        var iframe = document.createElement("iframe");
+        iframe.style.display = "none";
+        iframe.src = playStoreAppUrl;
+        document.body.appendChild(iframe);
+
+        setTimeout(function () {
+            document.body.removeChild(iframe);
+            // If Play Store didn't open, show Custom Download button
+            createButton("Get it on Google Play", playStoreUrl, "bg-green-600");
+        }, 2000);
+    } else {
+        createButton("Download for Other Devices", fallbackUrl, "bg-gray-600");
+    }
+};
